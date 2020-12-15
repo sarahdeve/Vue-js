f2a1baa33768a449e8ca5385be7c87cdf436fafc
Sarah <55793495+sarahdeve@users.noreply.github.com>
2020-12-15 23:24:41 +0100

    Vue food project

---
+++ vuefoodproject/src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <div id="App" >
+  
+    <Navbar/>
+    <HelloWorld/>
+    <!-- <Signup/> -->
+    <!-- <router-view></router-view> -->
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld';
+import Navbar from './components/navbar';
+// import Signup from './components/signup';
+
+export default {
+  name: 'App',
+  components: {
+   Navbar,
+   HelloWorld
+  //  Signup
+  }
+}
+</script>
+
+<style scope>
+#app {
+  
+}
+body {
+  background-image: url("./assets/imges/img2.jpg");
+ /* background-image opacity: 0.3 */
+    width: 100%;
+    height: auto;
+}
+</style>
